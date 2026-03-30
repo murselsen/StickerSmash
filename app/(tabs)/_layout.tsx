e7c38ccd570290e417e0cfe99c6d64bfa455d355
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-03-30 14:13:55 +0300

    GestureHandlerRootView eklendi; sekme düzeni güncellendi

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,39 +1,43 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 const TabLayout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "#ffd33d",
-        headerStyle: {
-          backgroundColor: "#25292e",
-        },
-        headerShadowVisible: false,
-        headerTintColor: "#fff",
-        tabBarStyle: {
-          backgroundColor: "#25292e",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />
-          ),
-          title: "Home",
+    <GestureHandlerRootView>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#ffd33d",
+          headerStyle: {
+            backgroundColor: "#25292e",
+          },
+          headerShadowVisible: false,
+          headerTintColor: "#fff",
+          tabBarStyle: {
+            backgroundColor: "#25292e",
+          },
         }}
-      />
-      <Tabs.Screen
-        name="about"
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="information-circle" size={size} color={color} />
-          ),
-          title: "About",
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
+            title: "Home",
+          }}
+        />
+        <Tabs.Screen
+          name="about"
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="information-circle" size={size} color={color} />
+            ),
+            title: "About",
+          }}
+        />
+      </Tabs>
+    </GestureHandlerRootView>
   );
 };
 
